23988716404db9a94dda8c90c490dd99becd0a39
gudlaraajesh <40784233+gudlaraajesh@users.noreply.github.com>
2023-02-22 17:47:47 +0000

    sets example

---
+++ sets_diff_country.py
@@ -0,0 +1,27 @@
+'''The first line contains an integer N, the total number of country stamps.
+The next N lines contains the name of the country where the stamp is from.'''
+
+'''
+Sample Input:
+
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France 
+
+Sample Output:
+
+5
+'''
+
+s = set()
+n = int(input())
+for i in range(n):
+    c = input()
+    s.add(c)
+
+print(len(s))
